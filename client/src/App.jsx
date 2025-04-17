5443924bdeb8bb3fba83e0f8e0dd55a57b3af8a1
Богачев Матвей <boga4iov.matvei2016@yandex.ru>
2025-04-18 00:05:41 +0400

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,36 @@
 import {Route, Routes} from "react-router-dom"
+import React, { useState } from 'react';
 import socketIO from 'socket.io-client'
-import Home from './componets/home/home.jsx'
-import ChatPage from './componets/chat/index.jsx'
-const socket = socketIO.connect("http://localhost:5000")
+import HomePage from './componets/home/home.jsx'
+import ChatPage from './componets/chat/chat.jsx'
+const socket = socketIO.connect("http://192.168.0.188:5000")
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('home');
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path="/chat" element={<ChatPage/>}></Route>
-    </Routes>
-  )
+    <div className="app">
+      {currentPage === 'home' && (
+        <HomePage
+          setCurrentPage={setCurrentPage}
+          setRoomId={setRoomId}
+          setUsername={setUsername}
+          socket={socket}
+        />
+      )}
+      {currentPage === 'chat' && (
+        <ChatPage
+          roomId={roomId}
+          username={username}
+          socket={socket}
+          setCurrentPage={setCurrentPage}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App
